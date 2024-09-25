559164997f9dab28c9f5d1840b22d2c8e1fccc8a
Julius Lopes <juliuscxlopes@gmail.com>
2024-09-24 21:03:12 -0300

    Meu primeiro commit

--- src/routes/attendants.js
+++ src/routes/attendants.js
@@ -9,7 +9,7 @@ const authenticateToken = require('../middleware/authMiddleware'); // Importa o 
 router.post('/register', authenticateToken, processAttendantData, attendantsController.registerAttendant); // Protege a rota de registro
 //router.get('/:email', authenticateToken, attendantsController.getAttendant);
 //router.put('/:email', authenticateToken, attendantsController.updateAttendant);
-r//outer.delete('/:email', authenticateToken, attendantsController.deleteAttendant);
+//outer.delete('/:email', authenticateToken, attendantsController.deleteAttendant);
 
 // Autenticação de login (sem middleware de autenticação)
 router.post('/login', processAttendantData, attendantsController.loginAttendant);
